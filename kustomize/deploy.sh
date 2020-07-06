4902f3b3529acb4260c726b037d6710e7f8e7020
Vilmos Nebehaj <v.nebehaj@gmail.com>
2020-07-06 15:57:20 -0700

    Add deployment manifests

---
+++ kustomize/deploy.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+export NAME=${NAME:-kip}
+export NAMESPACE=${NAME:-kip}
+export USE_REGION=${USE_REGION:-}
+export IMAGE_REGISTRY=${REGISTRY:-689494258501.dkr.ecr.us-east-1.amazonaws.com}
+export IMAGE_TAG=${TAG:-v0.1.0}
+export NFS_VOLUME_ENDPOINT=${NFS_VOLUME_ENDPOINT:-}
+export IMAGE_CACHE_CONTROLLER_REPLICAS=$([[ -n "${NFS_VOLUME_ENDPOINT}" ]] && echo -n 1 || echo -n 0)
+export CELL_SECURITY_GROUP=${CELL_SECURITY_GROUP:-}
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+[[ -n $USE_REGION ]] && echo "Deploying in region \"$USE_REGION\""
+
+[[ -n $CELL_SECURITY_GROUP ]] && EXTRA_SECURITY_GROUPS="[ $CELL_SECURITY_GROUP ]" || EXTRA_SECURITY_GROUPS="[]"
+export EXTRA_SECURITY_GROUPS
+
+kubectl version > /dev/null 2>&1 || {
+    echo "Missing kubectl or configuration problem"
+    exit 1
+}
+
+kustomize version > /dev/null 2>&1 || {
+    echo "Missing kustomize"
+    exit 1
+}
+
+envsubst --version > /dev/null 2>&1 || {
+    echo "Missing envsubst"
+    exit 1
+}
+
+kubectl get namespace "${NAMESPACE}" >/dev/null 2>&1 || kubectl create namespace "${NAMESPACE}"
+
+kustomize build "${SCRIPT_DIR}" | envsubst | kubectl apply -f -

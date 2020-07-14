105d741299cb01726528a52ea10e7cd4e2409f20
Vilmos Nebehaj <v.nebehaj@gmail.com>
2020-07-14 16:19:08 -0700

    Add build scripts

---
+++ build/build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+REGISTRY=${REGISTRY:-689494258501.dkr.ecr.us-east-1.amazonaws.com}
+TAG=${TAG:-$(git describe --dirty)}
+APPS="init-cert kip kube-proxy image-cache-controller kip-aws-meteringagent"
+
+for APP in $APPS; do
+    docker build -t ${REGISTRY}/elotl/${APP}:${TAG} -f Dockerfile.${APP} .
+    docker push ${REGISTRY}/elotl/${APP}:${TAG}
+done

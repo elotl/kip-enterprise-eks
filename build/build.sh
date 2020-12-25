3a3d84f3c6bd1893629093f6ed9b1cc27535c5e6
Vilmos Nebehaj <v.nebehaj@gmail.com>
2020-12-25 11:33:25 -0800

    Add kip-uptime

--- build/build.sh
+++ build/build.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 
 REGISTRY=${REGISTRY:-689494258501.dkr.ecr.us-east-1.amazonaws.com}
 TAG=${TAG:-$(git describe --dirty)}
-APPS="init-cert kip kube-proxy image-cache-controller kip-aws-meteringagent"
+APPS="init-cert kip kube-proxy image-cache-controller kip-aws-meteringagent kip-uptime"
 
 for APP in $APPS; do
     docker build -t ${REGISTRY}/elotl/${APP}:${TAG} -f Dockerfile.${APP} .
